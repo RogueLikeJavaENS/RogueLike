c990c0c85bf872f35bf899247a4c176c753eaca8
antoine <antoine.vidalma@gmail.com>
2021-02-19 15:34:11 +0100

    JColor Terminal Dependencies Docs in pom.xml

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,5 +1,8 @@
+import static com.diogonunes.jcolor.Ansi.*;
+import static com.diogonunes.jcolor.Attribute.*;
+
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println(colorize("This text will be yellow on magenta", YELLOW_TEXT(), MAGENTA_BACK()));
     }
 }
