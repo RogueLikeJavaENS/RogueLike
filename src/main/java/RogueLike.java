1eece3bffa5ac4524d9c565123a7d43f852d2bd0
antoine <antoine.vidalma@gmail.com>
2021-03-12 16:07:11 +0100

    Scan from panel

--- src/main/java/RogueLike.java
+++ src/main/java/RogueLike.java
@@ -19,19 +19,26 @@ public class RogueLike {
     /**
      * Create an instance of the game.
      */
-    RogueLike() {
+    RogueLike() throws InterruptedException {
         Seed seed = new Seed();
         Dungeon dungeon = DungeonStructure.createDungeon(seed);
         Player player = new Player(new Position(0,0),100, 100, "Hero", 1);
         GridMap gridMap = new GridMap(dungeon.getRoom(0));
         MiniMap miniMap = new MiniMap(dungeon);
         HUD hud = new HUD(player);
+        ScanPanel sp = new ScanPanel();
 
         while(true) {
             // print new GameState
-            RendererUI.roomRender(gridMap);
-            RendererUI.miniMapRender(miniMap);
-            RendererUI.hudRender(hud);
+            //RendererUI.roomRender(gridMap);
+            //RendererUI.miniMapRender(miniMap);
+            //RendererUI.hudRender(hud);
+            int a = 0;
+            while(a == 0) {
+                a = sp.getKeyPressed();
+                Thread.sleep(1);
+            }
+            System.out.println("pressed : "+a);
 
             // Pause Scan input
 
@@ -48,12 +55,13 @@ public class RogueLike {
             // Animation
 
             // Update GameState
-
+            Thread.sleep(2000);
+            sp.reset();
         }
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         RogueLike rogueLike = new RogueLike();
     }
 }
--- src/main/java/ScanPanel.java
+++ src/main/java/ScanPanel.java
@@ -4,14 +4,15 @@ import java.awt.event.KeyListener;
 import javax.swing.JFrame;
 
 public class ScanPanel extends JFrame implements KeyListener{
+    int pressed;
+
     public void keyPressed(KeyEvent e) {
-        int key = e.getKeyCode();
-        System.out.println(key);
     }
     public void keyReleased(KeyEvent e) {
-
+        pressed = e.getKeyCode();
     }
-    public void keyTyped(KeyEvent e) {}
+    public void keyTyped(KeyEvent e) {
+    }
 
     public ScanPanel() {
         addKeyListener(this);
@@ -21,6 +22,17 @@ public class ScanPanel extends JFrame implements KeyListener{
         setVisible(true);
         setSize(600,600);
     }
+
+    public int getKeyPressed() {
+        int a = pressed;
+        pressed = 0;
+        return a;
+    }
+
+    public void reset() {
+        pressed = 0;
+    }
+
     public static void main(String[] args) {
         ScanPanel t = new ScanPanel();
     }
