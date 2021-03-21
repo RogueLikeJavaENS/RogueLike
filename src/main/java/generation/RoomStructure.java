def66c61723a4c3ffbd3701e03760da38aebe680
Mr0B <luca.orengo@etu.univ-cotedazur.fr>
2021-03-21 10:56:24 +0100

    -Removed Magic Number from RoomStructure.createRoom.

--- src/main/java/generation/RoomStructure.java
+++ src/main/java/generation/RoomStructure.java
@@ -10,31 +10,7 @@ public class RoomStructure {
     public static Room createRoom(int current, Seed seed, int[] nextList) {
         int ROOM_WIDTH = 22;
         int ROOM_HEIGHT = 10;
-        //int[] nextList = InitNextlist();
-        /*switch (direction) {
-            case "north" : contents[0][6]=3;
-                break;
-            case "south" : contents[9][6]=3;
-                break;
-            case "east" : contents[6][0]=3;
-                break;
-            case "west" : contents[6][21]=3;
-        }*/
-//        for (int x = 0; x < 10; x++) {
-//            for (int y = 0; y < 22; y++) {
-//                if (x==0 || x==9 || y==0 || y==21){
-//                    if(contents[x][y]!=3) {
-//                        contents[x][y] = 1;
-//                    }
-//                    else{
-//                        contents[x][y] = 3;
-//                    }
-//                }
-//                else{
-//                    contents[x][y]=2;
-//                }
-//            }
-//        }
+
         Position roomPosition = new Position(nextList[5], nextList[4]); // using y x might need to reverse because i'm a dumbass
         int[][] contents;
         contents = new int[ROOM_HEIGHT][ROOM_WIDTH];
@@ -42,16 +18,16 @@ public class RoomStructure {
             if (nextList[z] != -1) {
                 switch (z) {
                     case 0:
-                        contents[0][6] = 3;
+                        contents[0][ROOM_WIDTH/2] = 3;
                         break;
                     case 1:
-                        contents[6][0] = 3;
+                        contents[ROOM_HEIGHT/2][ROOM_WIDTH-1] = 3;
                         break;
                     case 2:
-                        contents[9][6] = 3;
+                        contents[ROOM_HEIGHT-1][ROOM_WIDTH/2] = 3;
                         break;
                     case 3:
-                        contents[6][21] = 3;
+                        contents[ROOM_HEIGHT/2][0] = 3;
                         break;
                 }
             }
@@ -72,8 +48,3 @@ public class RoomStructure {
         return new Room(current, nextList, contents, roomPosition, ROOM_WIDTH, ROOM_HEIGHT);
     }
 }
-    /*private static int[] InitNextlist(){
-        int[] nextList = new int[4];
-        Arrays.fill(nextList, -1);
-        return nextList;
-    }*/
