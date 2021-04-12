c1e3d947f6327c0116aaa9b4cc50ec8c5dc3067d
Mr0B <luca.orengo@etu.univ-cotedazur.fr>
2021-04-12 18:02:15 +0200

    -Adding Monster xpWorth

--- src/main/java/entity/living/MonsterStats.java
+++ src/main/java/entity/living/MonsterStats.java
@@ -6,7 +6,19 @@ package entity.living;
  */
 
 public class MonsterStats extends AbstractStats {
-    public MonsterStats(int lifePoint, int manaPoint, int range, int rawDamage, int naturalArmor, int level) {
+
+    private int xpWorth;
+
+    public int getXpWorth() {
+        return xpWorth;
+    }
+
+    public void setXpWorth(int xpWorth) {
+        this.xpWorth = xpWorth;
+    }
+
+    public MonsterStats(int lifePoint, int manaPoint, int range, int rawDamage, int naturalArmor, int level, int xpWorth) {
         super(lifePoint, manaPoint, range, rawDamage, naturalArmor, level);
+        this.xpWorth = xpWorth;
     }
 }
