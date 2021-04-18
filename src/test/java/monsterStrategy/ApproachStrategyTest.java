cae360ce71ba4491e0196bab82815ea25b990f6a
Juliette Sabatier <juliette.sabatier07@gmail.com>
2021-04-18 16:15:44 +0200

    test

---
+++ src/test/java/monsterStrategy/ApproachStrategyTest.java
@@ -0,0 +1,55 @@
+package monsterStrategy;
+
+import display.GridMap;
+import entity.living.npc.monster.Monster;
+import entity.living.npc.monster.MonsterFactory;
+import entity.living.player.Player;
+import gameElement.Room;
+import generation.RoomFactory;
+import generation.RoomType;
+import generation.Seed;
+import org.junit.jupiter.api.Test;
+import utils.Position;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApproachStrategyTest {
+
+    @Test
+    void doAct() {
+    }
+
+    @Test
+    void updateStrategyDescription() {
+    }
+
+    @Test
+    void getStrategyDescription() {
+    }
+
+    @Test
+    void test() {
+        Condition condition = (Monster monster, Player player) -> true;
+        Strategy approach = new ApproachStrategy(condition,null);
+        RoomFactory roomFactory = new RoomFactory(15,15,1,2);
+        int[] tab = {1,2,3,4,5,6};
+        Room room = roomFactory.getRoom(new Seed(), RoomType.MONSTER,1,tab);
+        GridMap gridMap = new GridMap(room);
+        MonsterFactory monsterFactory = new MonsterFactory(1);
+        Position monsterPosition = new Position(1,2);
+        Position playerPosition1 = new Position(2,2);
+        Position playerPosition2 = new Position(4,4);
+        Position playerPosition3 = new Position(7,8);
+        Strategy skeletonStrategy = new IdleStrategy(monsterFactory.isFarFromPlayer, new ApproachStrategy(monsterFactory.alwaysTrue, new AttackStrategy(null)));
+        Monster monster = monsterFactory.getMonster(0,monsterPosition);
+        Player player = new Player(playerPosition1,10,10,"Hero",1);
+
+        assertFalse(approach.act(monster,player,gridMap));
+        player.setPosition(playerPosition2);
+        assertTrue(approach.act(monster,player,gridMap));
+        player.setPosition(playerPosition3);
+        assertTrue(approach.act(monster,player,gridMap));
+
+    }
+
+
+}
